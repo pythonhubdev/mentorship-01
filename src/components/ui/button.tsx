31f79c7bf182b4d059d75439d7a9a2bc9d1cf40f
Vetrichelvan <pythonhubdev@gmail.com>
2025-10-26 03:31:45 +0530

    feat: refactor UI components to use core utilities, update dependencies, and add new Label component

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -1,12 +1,11 @@
-import type { JSX, ValidComponent } from "solid-js";
-import { splitProps } from "solid-js";
-
 import * as ButtonPrimitive from "@kobalte/core/button";
 import type { PolymorphicProps } from "@kobalte/core/polymorphic";
 import type { VariantProps } from "class-variance-authority";
 import { cva } from "class-variance-authority";
+import type { JSX, ValidComponent } from "solid-js";
+import { splitProps } from "solid-js";
 
-import { cn } from "~/lib/utils";
+import { cn } from "~/core/lib/utils";
 
 const buttonVariants = cva(
 	"inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0",
